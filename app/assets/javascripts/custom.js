7c6a9a578be513cb7e1aeaae5bead6bc5d547bbe
Morgan Neiman <quackhouse@me.com>
2013-10-22 16:52:58 -0400

    added javascript fade fx

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,17 @@
+// $(function(){
+//   $('.lyric a').mouseover(function(){
+//     $(this).parent().siblings('.lyric-tool-tip').css('display', 'block');
+//   });
+//     $('.lyric a').mouseout(function(){
+//     $(this).parent().siblings('.lyric-tool-tip').css('display', 'none');
+//   });
+// });
+
 $(function(){
   $('.lyric a').mouseover(function(){
-    $(this).parent().siblings('.lyric-tool-tip').css('display', 'block');
+    $(this).parent().siblings('.lyric-tool-tip').fadeIn("400");
   });
     $('.lyric a').mouseout(function(){
-    $(this).parent().siblings('.lyric-tool-tip').css('display', 'none');
+    $(this).parent().siblings('.lyric-tool-tip').fadeOut("400");
   });
 });
