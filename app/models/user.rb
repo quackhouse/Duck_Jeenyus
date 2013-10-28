f0558c7116a537f3db53b71d0a1980953ae0d18a
Morgan Neiman <quackhouse@me.com>
2013-10-28 11:14:15 -0400

    fixed twitter login error from validating uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :access_token, :name, :location, :email, :password, :password_confirmation
-  validates_uniqueness_of :email, :message => "Sorry, that email's been taken."
+  validates_uniqueness_of :email, :allow_nil => true, :message => "Sorry, that email's been taken."
   has_many :comments
 end
