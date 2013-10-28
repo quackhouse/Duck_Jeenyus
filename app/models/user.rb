ad674a03d95bf4860b0cbd232d8cd244d44a98c8
Morgan Neiman <quackhouse@me.com>
2013-10-28 10:51:32 -0400

    added signup with email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_uniqueness_of :email, :message => "Sorry, that email's been taken."
   attr_accessible :access_token, :name, :location, :email, :password, :password_confirmation
   has_many :comments
 end
