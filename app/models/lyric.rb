a10327e71754a8730a965c023ea3734dd89a85c4
Morgan Neiman <quackhouse@me.com>
2013-10-28 14:08:13 -0400

    organized meanings by score in individual lyric display

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -1,4 +1,36 @@
 class Lyric < ActiveRecord::Base
   belongs_to :song
   has_many :comments
+
+  def sort_comments
+    comments = self.comments
+    @comments = []
+    comments.each do |comment|
+      votes_up = Vote.where(comment_id: comment.id, value: 1)
+      upvotes = 0
+      votes_up.each do |vote|
+        upvotes += 1
+      end
+      votes_down = Vote.where(comment_id: comment.id, value: 0)
+      downvotes = 0
+      votes_down.each do |vote|
+        downvotes += 1
+      end
+      score = upvotes - downvotes
+      lyric = Lyric.find(comment.lyric_id)
+      lyric_id = lyric.id
+      @comments << {
+        :comment_id => comment.id,
+        :comment_text => comment.text,
+        :upvotes => upvotes,
+        :downvotes => downvotes,
+        :score => score,
+        :lyric_id => lyric_id,
+        :song => lyric.song,
+        :lyric => lyric.text
+      }
+    end
+    @comments = @comments.sort_by { |hsh| hsh[:score] }
+    @comments.reverse!
+  end
 end
